1f7cdfbd184f3d4239c8770ebbedb6086880d7a5
Matuzalém Teles <matuzalemteles@gmail.com>
2015-10-14 21:56:18 -0300

    Exemplo de load da seed

--- v1/controllers/myController.php
+++ v1/controllers/myController.php
@@ -6,9 +6,11 @@
 	
 		private $load;
 		private $parameters;
+		private $seed;
 		
 		function __construct($parameters){
 			$this->load = $this->LoadModel('myModel');
+			$this->seed = $this->seeds('encryption');
 			$this->params = $parameters;
 		}
 		
@@ -20,14 +22,17 @@
 			if($result['num_rows'] == 1){
 				$Result = "sucess";
 				$Message = "Sucess when sending";
+				$Sha = $this->seed->sha512($Message);
 			}else{
 				$Result = "error";
 				$Message = "An unexpected error occurred";
+				$Sha = $this->seed->sha512($Message);
 			}
 			
 			$array = array(
 				"result" => $Result,
-				"message" => $Message
+				"message" => $Message,
+				"sha512" => $Sha
 			);
 			
 			$this->response($array);
