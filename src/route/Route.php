957b348203256f10341f7079f77cd0335e04833c
Matuzalém S. Teles <matuzalemteles@gmail.com>
2016-02-28 11:56:20 -0300

    Organizando espaçamento do Route

--- src/route/Route.php
+++ src/route/Route.php
@@ -7,50 +7,50 @@ use KepPHP\Kep\route\RouteFactory as Factory;
 class Route extends Group
 {
     /**
-         * POST interprets the communication and calls the controller if there.
-         *
-         * @acess public
-         */
-        public static function post($endpoint, $function)
-        {
-            $Factory = new Factory();
+     * POST interprets the communication and calls the controller if there.
+     *
+     * @acess public
+     */
+    public static function post($endpoint, $function)
+    {
+        $Factory = new Factory();
 
-            return $Factory->addRoute('POST', $endpoint, $function);
-        }
+        return $Factory->addRoute('POST', $endpoint, $function);
+    }
 
-        /**
-         * Interprets the GET communication and calls the controller if there.
-         *
-         * @acess public
-         */
-        public static function get($endpoint, $function)
-        {
-            $Factory = new Factory();
+    /**
+     * Interprets the GET communication and calls the controller if there.
+     *
+     * @acess public
+     */
+    public static function get($endpoint, $function)
+    {
+        $Factory = new Factory();
 
-            return $Factory->addRoute('GET', $endpoint, $function);
-        }
+        return $Factory->addRoute('GET', $endpoint, $function);
+    }
 
-        /**
-         * Interprets the PUT communication and calls the controller if there.
-         *
-         * @acess public
-         */
-        public static function put($endpoint, $function)
-        {
-            $Factory = new Factory();
+    /**
+     * Interprets the PUT communication and calls the controller if there.
+     *
+     * @acess public
+     */
+    public static function put($endpoint, $function)
+    {
+        $Factory = new Factory();
 
-            return $Factory->addRoute('PUT', $endpoint, $function);
-        }
+        return $Factory->addRoute('PUT', $endpoint, $function);
+    }
 
-        /**
-         * DELETE interprets the communication and calls the controller if there.
-         *
-         * @acess public
-         */
-        public static function delete($endpoint, $function)
-        {
-            $Factory = new Factory();
+    /**
+     * DELETE interprets the communication and calls the controller if there.
+     *
+     * @acess public
+     */
+    public static function delete($endpoint, $function)
+    {
+        $Factory = new Factory();
 
-            return $Factory->addRoute('DELETE', $endpoint, $function);
-        }
+        return $Factory->addRoute('DELETE', $endpoint, $function);
+    }
 }
