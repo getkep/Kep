c5145aaabbd72712dfeeee2a779f1117e36a8855
Matuzalém S. Teles <matuzalemteles@gmail.com>
2016-10-08 21:01:35 -0300

    Separating connection responsibilities.

---
+++ src/Database/MysqlConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace KepPHP\Kep\Database;
+
+use KepPHP\Kep\config\Config;
+
+class MysqlConnection
+{
+	/**
+     * Configuration.
+     *
+     * @var array
+     */
+	private $config;
+
+	/**
+     * Return connection settings.
+     *
+     * @return config
+     */
+	public function getConfig()
+	{
+		$this->config = config::getConfig();
+        $this->config = $this->config['connections'];
+
+        return $this;
+	}
+
+	/**
+     * Create a new database connection instance.
+     *
+     * @return MYSQLI
+     */
+	public function mysql
+	{
+		return new \mysqli($this->config['host'], $this->config['username'], $this->config['password'], $this->config['database']);
+	}
+}
